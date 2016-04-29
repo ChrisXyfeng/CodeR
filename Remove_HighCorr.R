52a2cfaa023adaaa0e4a34c177642373b47081e6
ChrisXyfeng <xyfengchris@gmail.com>
2016-04-29 10:29:48 +0200

    Update Remove_HighCorr.R

--- Remove_HighCorr.R
+++ Remove_HighCorr.R
@@ -1,5 +1,5 @@
 #Remove_HighCorr
 train_corr <- cor(train)
-high_corr <- findCorrelation(train_Corr, 0.90)
+high_corr <- findCorrelation(train_corr, 0.90)
 # returns an index of column numbers for removal
 train <- train[, -high_corr]
